95f8ca0ea88870751ae89c2ccedde1a76318c3e6
RonconiLudovico <ronconi.ludovico@gmail.com>
2025-01-22 10:18:53 +0100

    created both 'main.py' and 'test.py' updated 'README.md'

---
+++ main.py
@@ -0,0 +1,10 @@
+'''
+If data is empty then Return the empty list
+If the first element in data is a list then
+    Set l1 to the result of flattening the first element in data
+    Set l2 to the result of flattening all of the elements in data, except the first
+    Return the concatenation of l1 and l2
+If the first element in data is not a list then
+    Set l1 to a list containing only the first element in data
+    Set l2 to the result of flattening all of the elements in data, except the first Return the concatenation of l1 and l2
+'''
